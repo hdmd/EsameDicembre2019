0a531560a495a94c932f5e0d9a43bc863f46b183
hdmd <56116811+hdmd@users.noreply.github.com>
2020-01-17 22:13:07 +0100

    Update AppService.java
    Service revisionato

--- src/main/java/it/univpm/esameDicembre/Service/AppService.java
+++ src/main/java/it/univpm/esameDicembre/Service/AppService.java
@@ -1,6 +1,7 @@
 package it.univpm.esameDicembre.Service;
 
 import java.io.File;
+
 import java.lang.reflect.Method;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -16,8 +17,7 @@ import it.univpm.esameDicembre.Utils.Calculator;
 import it.univpm.esameDicembre.Utils.Utils;
 
 /**
- * Classe che ha la funzione di elaboare i dati e fornirli al Controller
- * per essere esposti verso l'utente.
+ * 
  * @author Mattia Dignani, Antonio Cozzolino
  *@version 1.0
  */
@@ -29,32 +29,38 @@ public class AppService {
 	private Calculator<Element> values = new Calculator<Element>();
 	private Filters<Element> filteredData = new Filters<Element>();
 	@Autowired
-	public AppService() throws IOException, NoSuchFieldException, SecurityException {
+	public AppService() throws IOException {
 		File f = new File("dataset.tsv");
 		if (!f.exists()) {
-			Utils.JSONDecode("https://data.europa.eu/euodp/data/api/3/action/package_show?id=3h1d8YdPl3KCgkeQNbjkA", "file.tsv");
+		Utils.JSONDecode("https://data.europa.eu/euodp/data/api/3/action/package_show?id=3h1d8YdPl3KCgkeQNbjkA", "file.tsv");
 		}
 		Utils.tsvParse(data, header, "file.tsv");
+	}
+	
+	
+	public ArrayList<Element> printdata() {
+		return data;
+	}
+	
+	public ArrayList<Metadata> printMetadata() {
+		return header;
 	}
 
 	public Element printElement(int index) {
 		if (index > data.size() || index < 0) {
 			throw new RuntimeException("Indice non valido");
-		}	
+		}
 		return data.get(index);
 	}
 
-	public ArrayList<Element> printElement() {
-		return data;
-	}
-
-	public Stats calculus(String field, String operation) { // throws NoSuchFieldException, SecurityException { 
+	
+	public Stats calculus(String field, String operation) {
 		try {
 			Filters.checkfield(field); 
 		} catch (NoSuchFieldException | SecurityException e) {
 			throw new RuntimeException ("Campo errato, riprovare");
 		}
-		return new Stats(operation, operation + " dei valori di " + field, values.calculation(data, field, operation));
+			return new Stats(operation, operation + " dei valori di " + field, values.calculation(data, field, operation));
 	}
 
 	public Stats avg(String field) {
@@ -65,28 +71,23 @@ public class AppService {
 		}
 		return new Stats("average", "Valore medio per " + field, values.calculation(data, field, "sum") / data.size());
 	}
-
-	public ArrayList<Metadata> printMetadata() {
-		return header;
-	}
-
-	public ArrayList<Stats> Stats(String field) {
+	
+	public ArrayList<Stats> Statistics(String field) {
 		try {
 			Filters.checkfield(field); 
 		} catch (NoSuchFieldException | SecurityException e) {
 			throw new RuntimeException ("Campo errato, riprovare");
 		}
-		ArrayList<Stats> statistics = new ArrayList<Stats>();
-		statistics.add(calculus(field,"sum"));
-		statistics.add(avg(field));
-		statistics.add(calculus(field,"min"));
-		statistics.add(calculus(field,"max"));
-		statistics.add(calculus(field,"devstd"));
-		return statistics;
+		ArrayList<Stats> stats = new ArrayList<Stats>();
+		stats.add(calculus(field,"sum"));
+		stats.add(avg(field));
+		stats.add(calculus(field,"min"));
+		stats.add(calculus(field,"max"));
+		stats.add(calculus(field,"devstd"));
+		return stats;
 	}
 
-	public HashMap<String, Integer> counter(String field) //metodo di tipo hashmap rappresentato da una chiave che indentifica una stringa e da un valore di tipo intero. Gli viene passato field come parametro.
-				throws NoSuchMethodException, RuntimeException, IllegalAccessException, ReflectiveOperationException {
+	public HashMap<String, Integer> counter(String field) throws NoSuchMethodException, RuntimeException, IllegalAccessException, ReflectiveOperationException {
 		try {
 			Filters.checkfield(field); 
 		} catch (NoSuchFieldException | SecurityException e) {
@@ -95,14 +96,13 @@ public class AppService {
 		ArrayList<String> inputColumn = new ArrayList<String>(); //creazione di un ArrayList da riempire.
 		int i; //indice di data.size(), usato per l'iterazione
 		for (i = 0; i < data.size(); i++) { //iterazione su tutti gli elementi di data
-			Method m = data.get(i).getClass()
-					.getMethod("get" + field.substring(0, 1).toUpperCase() + field.substring(1), null);  
+			Method m = data.get(i).getClass().getMethod("get" + field.substring(0, 1).toUpperCase() + field.substring(1), null);  
 			inputColumn.add((String) m.invoke(data.get(i)));
 		}
 		return Calculator.counter(inputColumn);
 	}
 	
-
+	
 	public ArrayList<Element> filter(String field, String operator, Object word_value) {
 		try {
 			Filters.checkfield(field); 
@@ -116,13 +116,8 @@ public class AppService {
  	}
 	
 
-	public ArrayList<Element> multifilter(String field, String logicOperator, String operator1, Object value1, String operator2, Object value2) {
-		try {
-			Filters.checkfield(field); 
-		} catch (NoSuchFieldException | SecurityException e) {
-			throw new RuntimeException ("Campo errato, riprovare");
-		}
-		if ((filteredData.checkup(operator1) && filteredData.checkup(operator2))) {	
+	public ArrayList<Element> multifiltervalue(String field, String logicOperator, String operator1, Object value1, String operator2, Object value2) {
+		if (filteredData.checkup(operator1) && filteredData.checkup(operator2)) {
 			ArrayList<Element> list1 = new ArrayList<Element>();
 			list1 = (ArrayList<Element>) filteredData.select(data, field, operator1, value1);
 			if (logicOperator.equals("and")) {
@@ -135,18 +130,12 @@ public class AppService {
 					throw new RuntimeException("Operatore logico non valido");
 				}
 			} else {
-				throw new RuntimeException("Operatore di confronto non valido. Inserire >, <, =");
-			}
-		  
-	}	
+			throw new RuntimeException("Operatori inseriti errati. Inserire >, <, =");
+		}
+	}
+	
 
 	public ArrayList<Element> multifilter(String field1, Object word, String field2, String operator, Float value) {
-		try {
-			Filters.checkfield(field1); 
-			Filters.checkfield(field2);
-		} catch (NoSuchFieldException | SecurityException e) {
-			throw new RuntimeException ("Uno dei due campo è errato, riprovare");
-		}
 		if (!filteredData.checkup(operator)) {
 			throw new RuntimeException("Operatore di confronto non valido. Operatori validi: >, <, =");
 		}
@@ -157,9 +146,9 @@ public class AppService {
 		return list2;
 	}
 	
-	public ArrayList<Element> addBody(Element body1) { 
-		data.add(body1);
-		return data;
+	public ArrayList<Element> addBody(Element body1) {
+		 data.add(body1);
+		 return data;
 	}
 	
 	public ArrayList<Element> deleteVal(int i) {
@@ -167,24 +156,20 @@ public class AppService {
 		if (i > data.size() || i < 0) {
 			throw new RuntimeException("Indice non valido");
 		} else {
-			deleteline.add(data.get(i));
+			deleteline.add( data.get(i) );
 		}
 		data.remove(i);
 		return deleteline;
 	}
 	
 	public ArrayList<Element> deleteMoreVal(String field, String operator, float value) {
-		try {
-			Filters.checkfield(field); 
-		} catch (NoSuchFieldException | SecurityException e) {
-			throw new RuntimeException ("Campo errato, riprovare");
-		}
 		if (!filteredData.checkup(operator)) {
 			throw new RuntimeException("Operatore di confronto non valido. Operatori validi: >, <, =");
 		} else {
 		ArrayList<Element> eliminatedData = new ArrayList<Element> ();
 		eliminatedData = (ArrayList<Element>) filteredData.select(data, field, operator, value);
-		data = (ArrayList<Element>) filteredData.merge2(data, eliminatedData);
+		
+		data = (ArrayList<Element>) filteredData.subtract(data, eliminatedData);
 		return data;	
 		}
 	}	
